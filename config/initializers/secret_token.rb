dd457b1ce9a22b180a585f16e91e2ef94cd1459c
Bruno Maia <queimadus@gmail.com>
2013-03-06 21:37:38 +0000

    First commit development&test environments set to sqlite3; production environment set to postgresql added gems: foundation and devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileMoneyEcosystem::Application.config.secret_token = '70049bf67c13b8fe383a853c222730bbd1a7b94cc430ddad34b7585d71ac352aee89161e8b199791a09c305b7314532566c66ed73929b38cc555142a158288ef'
