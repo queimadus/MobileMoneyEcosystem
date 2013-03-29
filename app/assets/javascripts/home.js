9111febd1540fd5517da4e8696ab97cd51c296ee
Bruno Maia <queimadus@gmail.com>
2013-03-29 02:57:02 +0000

    Exchanged foundation for twitter 3.0b framework. Created view for home, login, register-selection and general website layout.

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#register-link').click(fade_main_to_register_selection);
+});
+
+function fade_main_to_register_selection(){
+
+    $('#main #home-content').fadeOut(function(){
+        $('#main #register-selection').fadeIn();
+    });
+    return false;
+}
