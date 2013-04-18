9bfe88f1d338cd94bd84223bda434fa5fd302aec
João Santos <joaopedroasantos@gmail.com>
2013-04-18 15:31:31 +0100

    [AUTOMATED TESTING] Added add_product.rb, not completed

--- spec/acceptance/add_product.rb
+++ spec/acceptance/add_product.rb
@@ -3,7 +3,10 @@ require 'spec_helper'
 feature "Add product" do
 	Steps "Add a product correctly" do
 		When "I go to sign in page" do
-			page.visit "/products/new"
+			page.visit "/products"
+		end
+		And "I click 'Add new product'" do
+			page.find("btn btn-primary btn-large").click
 		end
 	end
 end
