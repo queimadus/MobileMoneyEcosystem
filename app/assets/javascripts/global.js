8d83f66398c1b105ea837bcf3492868f1bf82eac
Bruno Maia <queimadus@gmail.com>
2013-05-23 19:46:12 +0100

    qrfix

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -14,15 +14,19 @@ $(document).mouseup(function()
     $(".profile-dropdown-container").hide();
 });
 
-$(".menu-item").mouseenter(handlerIn).mouseleave(handlerOut);
-var menu_bar = $("#menu-bar");
-var act = $(".menu-item.active");
-handlerOut();
-menu_bar.show();
-function handlerIn(){
-   menu_bar.css("width",$(this).outerWidth()).css("left",$(this).position().left);
-}
 
-function handlerOut(){
-    menu_bar.delay(1300).css("width",act.outerWidth()).css("left",act.position().left);
+if($(".menu-item").length>0){
+    $(".menu-item").mouseenter(handlerIn).mouseleave(handlerOut);
+    var menu_bar = $("#menu-bar");
+    var act = $(".menu-item.active");
+    handlerOut();
+    menu_bar.show();
+
+    function handlerIn(){
+       menu_bar.css("width",$(this).outerWidth()).css("left",$(this).position().left);
+    }
+
+    function handlerOut(){
+        menu_bar.delay(1300).css("width",act.outerWidth()).css("left",act.position().left);
+    }
 }
