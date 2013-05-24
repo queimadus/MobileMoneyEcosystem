8e5eb3b790db6704e7470949d66dde7ac8642a13
João Carvalho <ei09028@fe.up.pt>
2013-05-24 06:29:19 +0100

    API Cart update e  added limits

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,14 @@ MobileMoneyEcosystem::Application.routes.draw do
   get "api/cart/completed"
   get "api/cart/clearcart"
   get "api/cart/allcarts"
-
-  match "api/products/:id" => "api/products#show"
   match "api/cart/:id" => "api/cart#show"
 
-  get "api/clientinfo/info"
+  get "api/limits/show"
+
+  get "api/client/info"
+
+  get "api/products/scan"
+  match "api/products/:id" => "api/products#show"
 
 
   devise_scope :user do
