c540aa0405281401291208995e664ebd3d5e8e5b
João Pedro Araújo Santos <joaopedroasantos@gmail.com>
2013-05-24 03:57:39 +0100

    Test products completed.

--- spec/functional/products/edit.rb
+++ spec/functional/products/edit.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 feature "Edit product" do
-	Steps "Edit a product correctly"
+	Steps "Edit a product correctly" do
 		When "I go to login page" do
 			page.visit "/login"
 		end
@@ -15,7 +15,19 @@ feature "Edit product" do
 			page.click_button "Sign in"
 		end
 		When "I go to products page" do
+			page.visit "/products/65/edit"
+		end
+		And "I edit product name" do
+			page.fill_in "product_name", :with => "TestProduct2"
+		end
+		And "I click on edit" do
+			page.click_link_or_button "product-submit"
+		end
+		And "I return to products page" do
 			page.visit "/products"
+		end
+		Then "Product should have its name changed" do
+			page.should have_content("TestProduct2")
 		end
 	end
 end
--- spec/functional/products/remove.rb
+++ spec/functional/products/remove.rb
@@ -1,25 +1,30 @@
 require 'spec_helper'
-include Warden::Test::Helpers
 
 feature "Remove product" do
-	before(:each) do
-		@u = User.new(:email => "m@m.m", :password => "bbbbbbbb")
-		@m = Merchant.new(:name => "Bruno")
-		@m.user = @u
-		login_as @m, :scope => :user
-	end
 	Steps "Remove a product correctly" do
+		When "I go to login page" do
+			page.visit "/login"
+		end
+		And "I fill in right email" do
+			page.fill_in "user_email", :with => "m@m.m"
+		end
+		And "I fill in  right password" do
+			page.fill_in "user_password", :with => "bbbbbbbb"
+		end
+		And "I click sign in" do
+			page.click_button "Sign in"
+		end
 		When "I go to products page" do
+			page.visit "/products/65/edit"
+		end
+		And "I click on delete" do
+			page.click_link_or_button "delete-product"
+		end
+		And "I return to products page" do
 			page.visit "/products"
 		end
-		And "I click edit product" do
-			page.click_link "/products/64/edit"
-		end
-		And "I click delete" do
-			page.click_link "/products/64"
-		end
-		Then "Page should no have product link" do
-			page.should_not have_content("/products/64")
+		Then "Product should not be on list" do
+			page.should_not have_content("TestProduct")
 		end
 	end
 end
